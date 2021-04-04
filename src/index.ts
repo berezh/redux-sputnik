702213e80130a876f16559ee5c22c1c86b9c8de6
verezh <verezh@gmail.com>
2021-04-04 16:28:14 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { ActionWith, newAction } from './action';
 import { newActionType } from './action-type';
-import { newReducer, newWorkerReducer } from './reducer';
+import { newReducer } from './reducer';
 import { setState } from './state';
 
-export { newReducer, newWorkerReducer, newAction, newActionType, setState, ActionWith };
+export { newReducer, newAction, newActionType, setState, ActionWith };
